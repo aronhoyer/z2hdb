d9ae3c2f0b76ff873b82c660156c0a7985db0f7b
Aron Høyer <aron@hoyer.com>
2025-12-31 13:16:22 +0100

    init

---
+++ flake.nix
@@ -0,0 +1,28 @@
+{
+  description = "LowLevel Academy Zero to Hero database project";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  };
+
+  outputs = { self, nixpkgs }:
+  let
+    systems = [ "x86_64-linux" "aarch64-linux" ];
+    forAllSystems = f: nixpkgs.lib.genAttrs systems (system:
+      f {
+        pkgs = import nixpkgs { inherit system; };
+      }
+    );
+  in
+  {
+    devShells = forAllSystems ({ pkgs }: {
+      default = pkgs.mkShell {
+        packages = with pkgs; [
+          gcc
+	  gnumake
+          clang-tools
+        ];
+      };
+    });
+  };
+}
